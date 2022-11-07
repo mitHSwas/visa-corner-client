d24390bd813af801c2ebf3e761f85f8ead59251d
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-11-08 00:59:25 +0600

    viva visa immegrent consulting service site setup with route is complte

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="text-3xl">Home is coming</div>
+        </div>
+    );
+};
+
+export default Home;
